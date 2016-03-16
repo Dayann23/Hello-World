2730d1565378829ccdfe5fd0c795d3301fa2d465
Dayann23 <dayawendy@hotmail.com>
2016-03-17 00:23:38 +0100

    Create Lib.java

---
+++ Lib.java
@@ -0,0 +1,29 @@
+/**
+ *
+ * @author ???
+ */
+
+public class Lib {
+
+    public void metodoAlumno1(){
+        System.out.println("metodo 1");
+        System.out.println("modificado por alumno 1"); // alumno 1
+    }
+
+    public void metodoAlumno2(){
+        System.out.println("metodo 2");
+        System.out.println("modificado por alumno 2"); // alumno 2
+
+    }
+
+    public void metodoComunitario(){
+
+        System.out.println("Aqui escribimos todos");
+// modificaciones alumno 1
+        System.out.println("Alumno 1 inserta esta sentencia");
+    }
+
+    public void metodo3 () { 
+        System.out.println("metodo 3");
+    }
+}
